05d0a266cbc1bd5aa68f246886f13efaaf7511ce
Jeremy Harris <jharris@rockharbor.org>
2012-03-05 15:11:45 -0800

    Initial structure

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>OC Easter</title>
+		<meta charset="utf-8" />
+		<meta name="title" content="OC Easter" />
+		<meta name="description" content="An Easter celebration of Jesus and His freedom at the OC Fairgrounds in Costa Mesa, California." />
+		<meta name="keywords" content="oc, orange county, easter, fairgrounds, francis chan, rockharbor, oc easter, pacific amphitheatre, freedom, jesus" />
+		<link rel="stylesheet" href="css/reset.css" />
+		<link rel="stylesheet" href="css/styles.css" />
+		<script src="js/jquery-1.7.1.min.js"></script>
+		<script src="js/jmpress.min.js"></script>
+		<script>
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-7415608-5']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+			
+			$(document).ready(function() {
+				$('#wrapper').jmpress();
+			})
+		</script>
+	</head>
+	<body>
+		<div id="wrapper">
+		</div>
+	</body>
+</html>
