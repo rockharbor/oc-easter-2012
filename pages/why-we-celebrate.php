4b3c09f115b94ff386422247053d4670d577836d
Jeremy Harris <jharris@rockharbor.org>
2012-03-19 08:43:44 -0700

    Updated content on why we celebrate

--- pages/why-we-celebrate.php
+++ pages/why-we-celebrate.php
@@ -3,12 +3,13 @@
 		<header>
 			<h1>Why We Celebrate</h1>
 		</header>
-		<p>The story of Easter was set in motion in a garden thousands of years ago with a man named Adam and a woman named Eve. Adam and Eve walked with God. They were complete in His presence. They had life and they had it to the full. It was how God intended it.</p>
-		<p>But the man and woman were deceived. They rebelled against God and, in an instant, were banished from the garden. The fullness of God&apos;s presence was gone as was the promise of life that would sustain forever.</p>
+		<p>The story of Easter begins with God. In the beginning, God created the world and everything in it. And as the crowning achievement of His creation, He created two human beings, a man named Adam and a woman Eve - the only created things that were said to be in the Creator&apos;s image.</p>
+		<p>God placed these two humans in a beautiful garden. It was His plan to always have them and their descendants dwell with Him. And in His presence in that garden, Adam and Eve were complete. They had life and they had it to the full. It was how God intended.</p>
+		<p>But the man and woman rebelled against God and, in an instant, were banished from the garden. The fullness of God&apos;s presence was gone and with it went life as God intended. The world became increasingly grim. </p>
 		<p>In that same instant, God, in His great love, initiated a plan to redeem the world and to free the people He created. He promised a Savior that would one day come and reclaim what was His.</p>
-		<p>Generations later, Jesus invaded the world. From His birth in a simple manger…to His teachings that He was the only way to God…to His life that offered an entirely new way of looking at the world, Jesus was a revolutionary. He was a threat to the established order of things.</p>
-		<p>Eventually, He was killed for being that threat. He was nailed to a cross and crucified as the redeeming sacrifice for all of humanity. But three days later, He rose from the dead…restoring the path to God and to life.</p>
-		<p>Through Jesus, we can have once again have freedom and life as God intended it.</p>
+		<p>Generations later, Jesus came into this world. From His birth in a simple manger to His teachings that He was the way, the truth, and the life to His offer of an entirely new way of looking at the world, Jesus was a revolutionary and a threat to the established order of things. But more than just a revolutionary, He was God in the flesh, here to dwell with His people.</p>
+		<p>Eventually, this Jesus gave up His life. He was nailed to a cross and crucified as the redeeming sacrifice for the rebelliousness of mankind. But three days later, He rose from the dead…restoring the path to life as it was intended.</p>
+		<p>And through this Jesus, our Savior, those who believe in Him can once again have freedom and life as God intended.</p>
 		<p class="orange">That&apos;s why we celebrate.</p>
 	</article>
 </section>
