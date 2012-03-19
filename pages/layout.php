186b5a9d2004d79afcaaf7b5ba3ccc24dd69269e
Jeremy Harris <jharris@rockharbor.org>
2012-03-19 09:03:45 -0700

    Removed accidentally commented tracking code

--- pages/layout.php
+++ pages/layout.php
@@ -13,7 +13,7 @@
 		<link rel="stylesheet" href="css/styles.css" />
 		<script src="js/jquery-1.7.1.min.js"></script>
 		<script>
-			/*var _gaq = _gaq || [];
+			var _gaq = _gaq || [];
 			_gaq.push(['_setAccount', 'UA-7415608-5']);
 			_gaq.push(['_trackPageview']);
 
@@ -21,7 +21,7 @@
 			 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 			 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();*/
+			})();
 			$(document).mousemove(function(event) {
 				var screenWidth = $(window).width();
 				var screenHeight = $(window).height();
