3519363e44fdcf06a01aa21c4f05f7dc9fe67911
Jeremy Harris <jharris@rockharbor.org>
2012-03-19 13:19:40 -0700

    Added content above recap video

--- pages/what-to-expect.php
+++ pages/what-to-expect.php
@@ -6,6 +6,7 @@
 		</header>
 		<p>The story of Easter will be told through a blend of dance, music, and teaching. Freedom and new life in Jesus will be celebrated through baptism, where a person is submerged and lifted out of water. This act is both a declarative statement to choose to follow Jesus, as well as a symbolic passage into the new life He offers.</p>
 		<p>Francis Chan, author of <em>Crazy Love: Overwhelmed by a Relentless God and Forgotten God: Reversing Our Tragic Neglect of the Holy Spirit</em>, will teach a message from the Bible. Francis founded Cornerstorne Community Church in Simi Valley and was the Senior Pastor there for 16 years. For more information on him and his ministry, <a href="http://francischan.org" target="_blank">check out his website</a>.</p>
+		<p>Check out this recap of last year's services for a glimpse into how the Easter story will be told and celebrated...</p>
 		<iframe src="http://player.vimeo.com/video/23031374?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="800" height="450" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 	</article>
 </section>
